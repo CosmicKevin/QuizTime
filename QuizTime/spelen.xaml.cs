84b7c239253b4fd27aa7381a2ce6bf7d81d36ae8
CosmicKevin <58428174+CosmicKevin@users.noreply.github.com>
2021-08-09 22:36:27 +0200

    Add files via upload

--- QuizTime/spelen.xaml.cs
+++ QuizTime/spelen.xaml.cs
@@ -1,18 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 using System.Windows.Threading;
 
 namespace QuizTime
@@ -25,7 +14,7 @@ namespace QuizTime
         public event EventHandler TimerReady;
 
         private int time;
-        private System.Threading.Timer timer;
+        private Timer timer;
         public spelen()
         {
             InitializeComponent();
